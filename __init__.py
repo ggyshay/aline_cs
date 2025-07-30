2fb14785046c43e2d7cb43c1aaaada717bf36fe1
yshay <ggyshay@gmail.com>
2025-07-29 22:42:26 -0300

    init comit

--- __init__.py
+++ __init__.py
@@ -1,15 +1,9 @@
-import importlib
 import logging
-import os
-import traceback
-
-from ableton.v2.base import listens
+from ableton.v3.control_surface import ElementsBase
 from ableton.v3.control_surface import (
     ControlSurface,
     ControlSurfaceSpecification,
-    create_skin,
 )
-
 from ableton.v3.control_surface.capabilities import (
     CONTROLLER_ID_KEY,
     NOTES_CC,
@@ -20,17 +14,15 @@ from ableton.v3.control_surface.capabilities import (
     inport,
     outport,
 )
+from Live.Clip import MidiNoteSpecification  # type: ignore
 
-from . import ara
-
-
-logger = logging.getLogger("ara")
+logger = logging.getLogger("aline")
 
 
 def get_capabilities():
     return {
         CONTROLLER_ID_KEY: controller_id(
-            vendor_id=5824,
+            vendor_id=589180224,
             product_ids=[1161],
             model_name=["ARAv2"],
         ),
@@ -38,240 +30,184 @@ def get_capabilities():
     }
 
 
-def create_mappings(control_surface):
-    mappings = {}
-
-    mappings["StepDecoder"] = dict(
-        matrix="steps_buttons",
-        instrument_matrix="instruments_buttons",
-        shift_button="shift_button",
-        mute_button="mute_button",
-        double_button="double_button",
-        page_matrix="page_buttons",
-        fill_button="fill_button",
-        encoder_matrix="encoder_matrix",
-    )
-
-    return mappings
+class Elements(ElementsBase):
+    def __init__(self, *a, **k):
+        super().__init__(*a, **k)
 
 
 class Specification(ControlSurfaceSpecification):
-    elements_type = ara.Elements
-    control_surface_skin = create_skin(skin=ara.Skin)
-    create_mappings_function = create_mappings
-    component_map = {
-        "Note_Editor": ara.NoteEditorComponent,
-        "StepDecoder": ara.StepDecoder,
-    }
+    elements_type = Elements
 
 
 def create_instance(c_instance):
-    return ARA(Specification, c_instance=c_instance)
+    return Nopia(Specification, c_instance=c_instance)
 
 
-class ARA(ControlSurface):
+class Nopia(ControlSurface):
     def __init__(self, *a, **k):
         super().__init__(*a, **k)
 
         self.log_level = "info"
 
-        self.start_logging()
+        self.show_message("Aline: Hello!")
+        logger.info("Aline: init started ...")
 
-        self.show_message("ARA: init mate")
-        logger.info("ARA: init started ...")
-        self.schedule_message(3, self.reflect_device)
+        self.selected_track = None
 
     def setup(self):
         super().setup()
-        self.init()
 
-    def reload_imports(self):
-        try:
-            importlib.reload(ara.sequencer)
-            importlib.reload(ara.note_editor)
-            importlib.reload(ara.decoder)
+    # def handle_tempo(self, msb, lsb):
+    #     tempo = (msb << 8 | lsb) / 256
+    #     logger.info(f"tempo: {tempo}")
+    #     self.song.tempo = tempo
 
-        except Exception as e:
-            exc = traceback.format_exc()
-            logging.warning(exc)
+    # def handle_scale(self, mode, scale):
+    #     logger.info(f"scale: {mode}, {scale}")
+    #     self.song.root_note = scale
+    #     self.song.scale_mode = True
+    #     self.song.scale_name = mode_to_name[mode]
 
-    def init(self):
-        self.reload_imports()
-        self.target_clip = None
-        self.target_clip_slot_index = None
-        self.target_track = None
-        self.target_device = None
-        logger.info("init started:")
-        with self.component_guard():
-            logger.info("   adding sking")
-            self._skin = create_skin(skin=ara.Skin, colors=ara.Rgb)
+    # def volume_bass(self, volume):
+    #     logger.info(f"bass volume: {volume}")
+    #     if self.bass_track is None:
+    #         self.show_message("Bass track not found")
+    #         return
 
-            logger.info("   adding listeners")
+    #     self.bass_track.mixer_device.volume.value = volume / 127
 
-            self._ARA__on_selected_track_changed.subject = self.song.view
+    # def volume_pad(self, volume):
+    #     logger.info(f"pad volume: {volume}")
+    #     if self.pad_track is None:
+    #         self.show_message("Pad track not found")
+    #         return
 
-            logger.info("   adding listeners done")
+    #     self.pad_track.mixer_device.volume.value = volume / 127
 
-            logger.info("creating components")
-            logger.info("   creating sequencer")
-            self.create_sequencer()
+    # def change_inputs(self):
+    #     if (self.pad_track is None) or (self.bass_track is None):
+    #         return
+    #     types = self.pad_track.available_input_routing_types
+    #     logger.info(f"types({len(types)}): {types}")
+    #     for i in range(len(types)):
+    #         logger.info(types[i].display_name)
+    #         if "Nopia Input" in types[i].display_name:
+    #             self.pad_track.input_routing_type = types[i]
+    #             self.bass_track.input_routing_type = types[i]
 
-            logger.info("   creating note editor")
-            self.create_note_editor()
+    #     channels = self.pad_track.available_input_routing_channels
+    #     logger.info(f"channels({len(channels)}): {channels}")
+    #     for i in range(len(channels)):
+    #         logger.info(channels[i].display_name)
+    #         if "Ch. 1" == channels[i].display_name:
+    #             self.bass_track.input_routing_channel = channels[i]
+    #             self.pad_track.input_routing_channel = channels[i + 1]
+    #             break
 
-            self._sequencer.set_editor(self._note_editor)
+    #     self.pad_track.arm = True
+    #     self.bass_track.arm = True
 
-            self.component_map["StepDecoder"].sequencer = self._sequencer
+    # def create_channels(self):
+    #     logger.info("creating channels")
 
-            logger.info(f" ara is enabled= {self._enabled}")
-            self.component_map["StepDecoder"].refresh_all_leds()
-            self.component_map["StepDecoder"].try_lock_callback = (
-                lambda: self.try_lock_track()
-            )
-            self.component_map["StepDecoder"].send_midi = self._send_midi
+    #     # Bass ======================
+    #     self.bass_track = self.song.create_midi_track(-1)
+    #     self.bass_track.name = "Bass (Nopia)"
+    #     self.bass_track.color = 0x00C4EDA9
+    #     for c in self.application.browser.user_library.iter_children:
+    #         if c.name == "Nopia":
+    #             for cc in c.iter_children:
+    #                 if "bass" in cc.name:
+    #                     self.application.browser.load_item(cc)
+    #                     break
+    #             break
 
-            self.get_target_track()
-            self.create_clip()
-            self.reflect_device()
+    #     # Pad ======================
+    #     self.pad_track = self.song.create_midi_track(-1)
+    #     self.pad_track.name = "Pad (Nopia)"
+    #     self.pad_track.color = 0x00C4EDA9
+    #     for c in self.application.browser.user_library.iter_children:
+    #         if c.name == "Nopia":
+    #             for cc in c.iter_children:
+    #                 if "pad" in cc.name:
+    #                     self.application.browser.load_item(cc)
+    #                     break
+    #             break
 
-    def get_target_track(self) -> None:
+    #     self.schedule_message(2, self.change_inputs)
+    def ensure_selected_track(self):
 
-        self.target_track = None
-        tracks = self.song.tracks
-        for track in tracks:
-            if track.name.lower() == "ara":
-                self.target_track = track
-                self._ARA__on_playing_clip_changed.subject = self.target_track
-                break
+        pass
 
-        self.create_clip()
-        self.reflect_device()
-
-    def try_lock_track(self):
-        logger.info(f"try lock track {self.song.view.selected_track.name}")
-        self.target_track = self.song.view.selected_track
-        self._ARA__on_playing_clip_changed.subject = self.target_track
-
-        self.create_clip()
-        self.reflect_device()
-
-    def assign_target_clip(self):
-        self._note_editor.set_clip(self.target_clip)
-        self._ARA__on_playhead_move.subject = self.target_clip
-        self.try_grab_device()
-        self._sequencer.clear_all()
-        self._sequencer.syncState()
-
-    def create_clip(self):
-        if self.target_track is None:
+    def create_clip(self, length=1):
+        # self.clip = self.song.view.selected_track.clip_slots[0].create_clip(length)
+        selected_track = self.song.view.selected_track
+        i = 0
+        while selected_track.clip_slots[i].has_clip:
+            i += 1
+        if i >= len(selected_track.clip_slots):
+            self.show_message("No more clip slots available")
             return
+        self.clip = selected_track.clip_slots[i].create_clip(length/4)
+        selected_track.clip_slots[i].fire()
 
-        logger.info(f"Target track: {self.target_track.name}")
-        self._ARA__on_devices_changed.subject = self.target_track
 
-        slot = self.target_track.clip_slots[0]
-        if slot.has_clip:
-            self.target_clip = slot.clip
-            self.target_clip.fire()
-        else:
-            self.target_clip = slot.create_clip(4)
-            slot.fire()
 
-        self.assign_target_clip()
-
-        logger.info("creating clip done")
-
-    def try_grab_device(self):
-        if self.target_track is None:
-            self.target_device = None
-            return
-
-        logger.info(f"devices={self.target_track.devices}")
-        device = self.target_track.devices[0]
-
-        logger.info(f"pad={device.drum_pads[36]}")
-        logger.info(f"pad={device.drum_pads[36].chains}")
-        logger.info(f"pad={device.drum_pads[36].chains[0]}")
-        logger.info(f"pad={device.drum_pads[36].chains[0].mixer_device}")
-        logger.info(f"pad={device.drum_pads[36].chains[0].mixer_device.volume}")
-
-        if device.can_have_drum_pads:
-            self.target_device = device
-            self._sequencer.set_device(device)
-            self.component_map["StepDecoder"].set_device(device)
-
-    def reflect_device(self):
-        logger.info("reflecting device")
-        if getattr(self._sequencer, "_device", None) is None:
-            return
-        self._sequencer.syncState()
-        self._send_midi((0x90, 0, 127))
-        self.schedule_message(1, self.component_map["StepDecoder"].refresh_all_leds)
-
-    def create_note_editor(self):
-        self._note_editor = ara.NoteEditorComponent(
-            is_enabled=False,
-            sequencer_clip=self.target_clip,
-            sequencer=self._sequencer,
+    def add_note(
+        self,
+        start,
+        duration,
+        pitch,
+        vel,
+    ):
+        logger.info(f"add note: {start}, {duration}, {pitch}, {vel}")
+        note = MidiNoteSpecification(
+            pitch=pitch,
+            start_time=start/4,
+            duration=duration/4,
+            velocity=vel,
+            mute=False,
         )
+        self.clip.add_new_notes((note,))
+        self.clip.deselect_all_notes()
 
-    def create_sequencer(self):
-        self._sequencer = ara.Sequencer()
-
-    def start_logging(self):
-        """
-        Start logging to a local logfile (logs/abletonosc.log),
-        and relay error messages via OSC.
-        """
-        module_path = os.path.dirname(os.path.realpath(__file__))
-        log_dir = os.path.join(module_path, "logs")
-        if not os.path.exists(log_dir):
-            os.mkdir(log_dir, 0o755)
-        log_path = os.path.join(log_dir, "ara.log")
-        self.log_file_handler = logging.FileHandler(log_path)
-        self.log_file_handler.setLevel(self.log_level.upper())
-        formatter = logging.Formatter("(%(asctime)s) [%(levelname)s] %(message)s")
-        self.log_file_handler.setFormatter(formatter)
-        logger.addHandler(self.log_file_handler)
-
-    def stop_logging(self):
-        logger.removeHandler(self.log_file_handler)
+    def process_midi_bytes(self, data, midi_processor):
+        logger.info(f"midi bytes: {data}")
+        self.ensure_selected_track()
+        if data[0] != 0xF0:
+            return
+        if data[1] != 0x13:
+            self.create_clip(data[2])
+            return
+        # {
+        #                              0xF0,
+        #                              0x13,
+        #                              (byte)(start & 0xFF),
+        #                              (byte)((start >> 8) & 0xFF),
+        #                              (byte)((start >> 16) & 0xFF),
+        #                              (byte)((start >> 24) & 0xFF),
+        #                              (byte)(duration & 0xFF),
+        #                              (byte)((duration >> 8) & 0xFF),
+        #                              (byte)((duration >> 16) & 0xFF),
+        #                              (byte)((duration >> 24) & 0xFF),
+        #                              pitch,
+        #                              vel,
+        #                              0xF7};
+        start = data[2] | (data[3] << 7) | (data[4] << 14) | (data[5] << 21)
+        duration = data[6] | (data[7] << 7) | (data[8] << 14) | (data[9] << 21)
+        duration = duration/16.0
+        pitch = data[10]
+        vel = data[11]
+        logger.info(f"start: {start}, duration: {duration}, pitch: {pitch}, vel: {vel}")
+        self.add_note(
+            start,
+            duration,
+            pitch,
+            vel,
+        )
 
     def disconnect(self):
         self.show_message("Disconnecting...")
         logger.info("Disconnecting...")
         self.stop_logging()
         super().disconnect()
-
-    @listens("selected_track")
-    def __on_selected_track_changed(self):
-        logger.info(f"selected track changed: {self.song.view.selected_track.name}")
-
-    @listens("playing_position")
-    def __on_playhead_move(self):
-
-        if self.target_clip is not None:
-            self._sequencer.set_position(self.target_clip.playing_position)
-
-    @listens("devices")
-    def __on_devices_changed(self):
-        self.try_grab_device()
-
-    @listens("playing_slot_index")
-    def __on_playing_clip_changed(self):
-        if self.target_track is None:
-            return
-        if self.target_track.playing_slot_index < 0:
-            self.target_clip = None
-            return
-
-        if self.target_track.playing_slot_index == self.target_clip_slot_index:
-            return
-
-        self.target_clip_slot_index = self.target_track.playing_slot_index
-        logger.info(f"playing clip changed: {self.target_clip_slot_index}")
-
-        self.target_clip = self.target_track.clip_slots[
-            self.target_clip_slot_index
-        ].clip
-        self.assign_target_clip()
